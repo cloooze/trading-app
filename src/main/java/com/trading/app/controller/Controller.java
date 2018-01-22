1c97e832182fc426f9962b02a8b7ac49adda2a42
Braghese Andrea <a.braghese@reply.it>
2018-01-22 18:03:58 +0100

    changed some http methods

--- src/main/java/com/trading/app/controller/Controller.java
+++ src/main/java/com/trading/app/controller/Controller.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +15,23 @@ import com.trading.app.entity.Coin;
 import com.trading.app.service.CoinService;
 
 @RestController
-@RequestMapping("/trading/")
+@RequestMapping("/rest/db/")
 public class Controller {
 	
 	@Autowired
 	CoinService service;
 	
-	@GetMapping(value = "/save/{name}/{value}")
-	public Coin saveCoin(@PathVariable final String name, @PathVariable final Long value) {
-		return service.save(new Coin(name, value));
-	}
-	
-	@GetMapping(value = "/get/{name}")
-	public Coin getCoin(@PathVariable final String name) {
+	@GetMapping(value = "/{name}")
+	public Coin get(@PathVariable final String name) {
 		return service.findByName(name);
 	}
+	
+	@PostMapping(value = "/add")
+	public Coin add(@RequestBody final Coin coin) {
+		return service.save(coin);
+	}
+	
+	
 	
 	@GetMapping(value = "/get")
 	public List<String> getAll() {
