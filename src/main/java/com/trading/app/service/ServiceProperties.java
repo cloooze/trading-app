de6cd18601e6e86a6a877e24ddecb757a73ae623
andrea <cloooze@gmail.com>
2018-01-23 23:51:12 +0100

    added @validator to service config class

--- src/main/java/com/trading/app/service/ServiceProperties.java
+++ src/main/java/com/trading/app/service/ServiceProperties.java
@@ -2,15 +2,16 @@ package com.trading.app.service;
 
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 /*
  * demonstrates how service-specific properties can be injected
  */
 @ConfigurationProperties(prefix = "custom.app.test", ignoreUnknownFields = false)
 @Component
+@Validated
 public class ServiceProperties {
 
     @NotNull // you can also create configurationPropertiesValidator
